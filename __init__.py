bb4d0db414af5e50bf53fb9f9af3e656edf88ad8
Xiaohan Lu <luxh@ihep.ac.cn>
2019-05-28 17:33:47 +0200

    Add import statement in __init__.py in pysixdesk folder to import all the modules via 'from pysixdesk import *', add message method in utils

---
+++ __init__.py
@@ -0,0 +1,8 @@
+#import all the modules in pysixdesk
+from pysixdesk.lib import utils
+from pysixdesk.lib import gather
+from pysixdesk.lib import sixtrack
+from pysixdesk.lib import preprocess
+from pysixdesk.lib.study import Study
+from pysixdesk.lib.pysixdb import SixDB
+from pysixdesk.lib.submission import Cluster, HTCondor
--- lib/utils.py
+++ lib/utils.py
@@ -4,13 +4,17 @@ import re
 import sys
 import gzip
 import shutil
+from datetime import datetime
+
+#Gobal variable
+MES_TYPE = dict([[0,'Message'], [1, 'Warning'], [2, 'Error']])
 
 def check(files):
     '''Check the existence of the files and rename them if the files is a dict
     which looks like {'file1_oldName': 'file1_newName',
     'file2_oldName': 'file2_newName'}
     '''
-    lStatus = True
+    status = False
     if isinstance(files, dict):
         for key,value in files.items():
             if os.path.isfile(key):
@@ -18,22 +22,23 @@ def check(files):
                     os.rename(key, value)
             else:
                 print("The file %s isn't generated successfully!" %key)
-                lStatus = False
+                return status
     elif isinstance(files, list):
         for key in files:
             if not os.path.isfile(key):
                 print("The file %s isn't generated successfully!" %key)
-                lStatus = False
+                return status
     else:
         print("The input must be a list or dict!")
-        lStatus = False
-    return lStatus
+        return status
+    status = True
+    return status
 
 def download_output(filenames, dest, zp=True):
     '''Download the requested files to the given destinaion.
     If zp is true, then zip the files before download.
     '''
-    lStatus = True
+    status = False
     if not os.path.isdir(dest):
         os.makedirs(dest, 0o755)
 
@@ -47,14 +52,15 @@ def download_output(filenames, dest, zp=True):
                 shutil.copy(filename, dest)
         else:
             print("The file %s doesn't exist, download failed!"%filename)
-            lStatus = False
-    return lStatus
+            return status
+    status = True
+    return status
 
 def replace(patterns, replacements, source, dest):
     '''Reads a source file and writes the destination file.
     In each line, replaces patterns with repleacements.
     '''
-    lStatus = True
+    status = False
     if os.path.isfile(source):
         fin = open(source, 'r')
         fout = open(dest, 'w')
@@ -67,25 +73,27 @@ def replace(patterns, replacements, source, dest):
         fout.close()
     else:
         print("The file %s doesn't exist!"%source)
-        lStatus = False
-    return lStatus
+        return status
+    status = True
+    return status
 
 def encode_strings(inputs):
     '''Convert list or directory to special-format string'''
-    lStatus = True
+    status = False
     if isinstance(inputs, list):
         output = ','.join(map(str, inputs))
     elif isinstance(inputs, dict):
         a = [':'.join(map(str, i)) for i in inputs.items()]
         output = ','.join(map(str, a))
     else:
-        lStatus = False
         output = ''
-    return lStatus, output
+        return status, output
+    status = True
+    return status, output
 
 def decode_strings(inputs):
     '''Convert special-format string to list or directory'''
-    lStatus = True
+    status = False
     if isinstance(inputs, str):
         if ':' in inputs:
             output = {}
@@ -97,14 +105,15 @@ def decode_strings(inputs):
             output = inputs.split(',')
     else:
         print("The input is not string!")
-        lStatus = False
         output = []
-    return lStatus, output
+        return status, output
+    status = True
+    return status, output
 
 def compress_buf(data, source='file'):
     '''Data compression for storing in database
     The data source can be file,gzip,str'''
-    lStatus = True
+    status = False
     zbuf = io.BytesIO()
     if source is 'file' and os.path.isfile(data):
         with gzip.GzipFile(mode='wb', fileobj=zbuf) as zfile:
@@ -119,13 +128,14 @@ def compress_buf(data, source='file'):
         with gzip.GzipFile(mode='wb', fileobj=zbuf) as zfile:
             zfile.write(buf)
     else:
-        lStatus = False
         print("Invalid data source!")
-    return lStatus, zbuf.getvalue()
+        return status, zbuf.getvalue()
+    status = True
+    return status, zbuf.getvalue()
 
 def decompress_buf(buf, out, des='file'):
     '''Data decompression to retireve from database'''
-    lStatus = True
+    status = False
     if isinstance(buf, bytes):
         zbuf = io.BytesIO(buf)
         if des is 'file':
@@ -137,13 +147,31 @@ def decompress_buf(buf, out, des='file'):
                 out = f_in.read()
                 out = out.decode()
         else:
-            lStatus = False
             print("Unknow output type!")
-        return lStatus, out
+            return status
+        status = True
+        return status, out
     else:
-        lStatus = False
         print("Invalid input data!")
-        return lStatus
+        return status
+
+def message(meslevel, ty, content, log=False, level=1, **kwargs):
+    '''Print the different type message to the destination'''
+    if meslevel <= level:
+        now = datetime.now()
+        date_time = now.strftime("%a %d/%b/%Y %H:%M:%S")
+        if ty in MES_TYPE.keys():
+            mes_type = MES_TYPE[ty]
+        else:
+            mes_type = 'Unknow'
+        message = "%s %s: %s"%(date_time, mes_type, content)
+        if log and 'log_file' in kwargs.keys():
+            log_file = kwargs['log_file']
+            with open(log_file, 'a') as f_out:
+                f_out.write(message)
+                f_out.write('\n')
+        else:
+            print(message)
 
 def evlt(fun, inputs, action=sys.exit):
     '''Evaluate the specified function'''
