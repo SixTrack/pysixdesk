459223a44833a00e2d917a1d052387f7fd8ef222
Loic Coyle <loic.coyle@hotmail.fr>
2019-07-30 17:23:17 +0200

    added Path for initial import

--- tests/integration/sql/test_sql.py
+++ tests/integration/sql/test_sql.py
@@ -3,8 +3,9 @@ import os
 import shutil
 import time
 import sys
+from pathlib import Path
 # give the test runner the import access
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
+sys.path.insert(0, Path(__file__).parents[2].absolute())
 import pysixdesk
 from pathlib import Path
 
